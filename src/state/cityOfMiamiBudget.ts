6331bb5e696ef28f4152776d9fd2629ebddeb4c7
Nick <nick@nickmccrea.com>
2020-04-24 19:59:42 -0400

    Use and type `createSlice`.

--- src/state/cityOfMiamiBudget.ts
+++ src/state/cityOfMiamiBudget.ts
@@ -5,41 +5,7 @@
  */
 
 import { RootState, AppThunk } from "./store"
-
-// Actions
-const FETCH_DATA_REQUEST = "FETCH_DATA_REQUEST"
-interface FetchDataRequestAction {
-  type: typeof FETCH_DATA_REQUEST
-}
-
-const fetchDataRequest = (): FetchDataRequestAction => ({
-  type: FETCH_DATA_REQUEST,
-})
-
-const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS"
-interface FetchDataSuccessAction {
-  type: typeof FETCH_DATA_SUCCESS
-  payload: {
-    data: object
-  }
-}
-
-const fetchDataSuccess = (data: object): FetchDataSuccessAction => ({
-  type: FETCH_DATA_SUCCESS,
-  payload: {
-    data,
-  },
-})
-
-type CityOfMiamiBudgetAction = FetchDataRequestAction | FetchDataSuccessAction
-
-// Async Thunks
-const fetchData = (url: string): AppThunk => async (dispatch) => {
-  dispatch(fetchDataRequest())
-  const response = await fetch(url)
-  const data = await response.json()
-  dispatch(fetchDataSuccess(data))
-}
+import { createSlice, CaseReducer, PayloadAction } from "@reduxjs/toolkit"
 
 // State
 enum Status {
@@ -55,36 +21,22 @@ interface CityOfMiamiBudgetState {
 
 const INITIAL_STATE: CityOfMiamiBudgetState = { status: Status.Idle }
 
-// Reducers
-const reduceFetchDataRequest = (
-  previousState: CityOfMiamiBudgetState
-): CityOfMiamiBudgetState => ({
+// Case Reducers
+const fetchDataRequest: CaseReducer<CityOfMiamiBudgetState, PayloadAction> = (
+  previousState
+) => ({
   ...previousState,
   status: Status.Loading,
 })
 
-const reduceFetchDataSuccess = (
-  previousState: CityOfMiamiBudgetState,
-  action: FetchDataSuccessAction
-): CityOfMiamiBudgetState => ({
+const fetchDataSuccess: CaseReducer<
+  CityOfMiamiBudgetState,
+  PayloadAction<object>
+> = (previousState, action) => ({
   ...previousState,
   status: Status.Success,
   data: action.payload,
 })
-
-const reducer = (
-  previousState = INITIAL_STATE,
-  action: CityOfMiamiBudgetAction
-): CityOfMiamiBudgetState => {
-  switch (action.type) {
-    case FETCH_DATA_REQUEST:
-      return reduceFetchDataRequest(previousState)
-    case FETCH_DATA_SUCCESS:
-      return reduceFetchDataSuccess(previousState, action)
-    default:
-      return previousState
-  }
-}
 
 // Selectors
 const getStatus = (state: RootState) =>
@@ -92,6 +44,24 @@ const getStatus = (state: RootState) =>
 
 const getData = (state: RootState) =>
   (state.cityOfMiamiBudget && state.cityOfMiamiBudget.data) || undefined
+
+// Slice
+const { actions, reducer } = createSlice({
+  name: "cityOfMiamiBudget",
+  initialState: INITIAL_STATE,
+  reducers: {
+    fetchDataRequest,
+    fetchDataSuccess,
+  },
+})
+
+// Async Thunks
+const fetchData = (url: string): AppThunk => async (dispatch) => {
+  dispatch(actions.fetchDataRequest())
+  const response = await fetch(url)
+  const data = await response.json()
+  dispatch(actions.fetchDataSuccess(data))
+}
 
 export default reducer
 export { fetchData, getStatus, getData, Status }
