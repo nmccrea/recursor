f576c4352c2e92c96f196f56a98cb13ae20cd333
Nick <nick@nickmccrea.com>
2020-04-27 17:15:30 -0400

    Allow Jest to work with TypeScript.

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,4 +1,5 @@
 const babelOptions = {
-  presets: ["babel-preset-gatsby"],
+  presets: ["babel-preset-gatsby", "@babel/preset-typescript"],
 }
+
 module.exports = require("babel-jest").createTransformer(babelOptions)
