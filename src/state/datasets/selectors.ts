6de243b64c85efb4a02bf9538127a344ca38c889
Nick <nick@nickmccrea.com>
2020-05-05 17:53:26 -0400

    Remove bad memoization.

--- src/state/datasets/selectors.ts
+++ src/state/datasets/selectors.ts
@@ -4,26 +4,24 @@
  * TODO: doc
  */
 
-import { createSelector } from "@reduxjs/toolkit"
 import { RootState } from "../store"
 import { DatasetId } from "./types"
 import entityAdapter from "./entityAdapter"
 
 const selectDatasets = (state: RootState) => state.datasets
 
-const selectorForDataset = (datasetId: DatasetId) => (state: RootState) =>
-  entityAdapter.getSelectors(selectDatasets).selectById(state, datasetId)
+const selectorForAsyncState = (datasetId: DatasetId) => (state: RootState) => {
+  const dataset = entityAdapter
+    .getSelectors(selectDatasets)
+    .selectById(state, datasetId)
+  return (dataset && dataset.asyncState) || undefined
+}
 
-const selectorForAsyncState = (datasetId: DatasetId) =>
-  createSelector(
-    selectorForDataset(datasetId),
-    (dataset) => (dataset && dataset.asyncState) || undefined
-  )
-
-const selectorForData = (datasetId: DatasetId) =>
-  createSelector(
-    selectorForDataset(datasetId),
-    (dataset) => (dataset && dataset.data) || undefined
-  )
+const selectorForData = (datasetId: DatasetId) => (state: RootState) => {
+  const dataset = entityAdapter
+    .getSelectors(selectDatasets)
+    .selectById(state, datasetId)
+  return (dataset && dataset.data) || undefined
+}
 
 export { selectorForAsyncState, selectorForData }
