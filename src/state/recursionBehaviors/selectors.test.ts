fafe3bd4e33a306d534cff3b2d814c7848682e2d
Nick <nick@nickmccrea.com>
2020-05-15 21:47:26 -0400

    Implement needed selectors.

--- src/state/recursionBehaviors/selectors.test.ts
+++ src/state/recursionBehaviors/selectors.test.ts
@@ -1,4 +1,117 @@
-import { selectorForRecursionBehavior } from "./selectors"
+import {
+  selectAll,
+  selectIds,
+  selectorForRecursionBehavior,
+  selectorForTranslation,
+  selectorForScale,
+  selectorForAngle,
+  selectorForDepth,
+} from "./selectors"
+
+describe("selectAll()", () => {
+  it("returns a list of all existing recursion behaviors, ordered according to the `ids` state", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/a", "test/c", "test/b"],
+        entities: {
+          "test/a": {
+            id: "test/a",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/b": {
+            id: "test/b",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+          "test/c": {
+            id: "test/c",
+            color: "yellow",
+            translation: 0.1,
+            scale: 0.02,
+            angle: -Math.PI / 0.9,
+            depth: 2,
+          },
+        },
+      },
+    }
+
+    const result = selectAll(state)
+
+    expect(result).toEqual([
+      {
+        id: "test/a",
+        color: "blue",
+        translation: 1,
+        scale: 0.7,
+        angle: Math.PI / 3,
+        depth: 5,
+      },
+      {
+        id: "test/c",
+        color: "yellow",
+        translation: 0.1,
+        scale: 0.02,
+        angle: -Math.PI / 0.9,
+        depth: 2,
+      },
+      {
+        id: "test/b",
+        color: "red",
+        translation: 0.8,
+        scale: 0.5,
+        angle: -Math.PI / 4,
+        depth: 7,
+      },
+    ])
+  })
+})
+
+describe("selectIds()", () => {
+  it("returns the ordered list of IDs of all existing recursion behaviors", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/a", "test/b", "test/c"],
+        entities: {
+          "test/a": {
+            id: "test/a",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/b": {
+            id: "test/b",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+          "test/c": {
+            id: "test/c",
+            color: "yellow",
+            translation: 0.1,
+            scale: 0.02,
+            angle: -Math.PI / 0.9,
+            depth: 2,
+          },
+        },
+      },
+    }
+
+    const result = selectIds(state)
+
+    expect(result).toEqual(["test/a", "test/b", "test/c"])
+  })
+})
 
 describe("selectorForRecursionBehavior()", () => {
   it("returns a selector which returns the identified recursion behavior", () => {
@@ -36,5 +149,133 @@ describe("selectorForRecursionBehavior()", () => {
       angle: Math.PI / 3,
       depth: 5,
     })
+  })
+})
+
+describe("selectorForTranslation()", () => {
+  it("returns a selector which returns the identified recursion behavior's translation", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/subject", "test/control"],
+        entities: {
+          "test/subject": {
+            id: "test/subject",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/control": {
+            id: "test/control",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+        },
+      },
+    }
+
+    const result = selectorForTranslation("test/subject")(state)
+
+    expect(result).toEqual(1)
+  })
+})
+
+describe("selectorForScale()", () => {
+  it("returns a selector which returns the identified recursion behavior's scale", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/subject", "test/control"],
+        entities: {
+          "test/subject": {
+            id: "test/subject",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/control": {
+            id: "test/control",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+        },
+      },
+    }
+
+    const result = selectorForScale("test/subject")(state)
+
+    expect(result).toEqual(0.7)
+  })
+})
+
+describe("selectorForAngle()", () => {
+  it("returns a selector which returns the identified recursion behavior's angle", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/subject", "test/control"],
+        entities: {
+          "test/subject": {
+            id: "test/subject",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/control": {
+            id: "test/control",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+        },
+      },
+    }
+
+    const result = selectorForAngle("test/subject")(state)
+
+    expect(result).toEqual(Math.PI / 3)
+  })
+})
+
+describe("selectorForDepth()", () => {
+  it("returns a selector which returns the identified recursion behavior's depth", () => {
+    const state = {
+      recursionBehaviors: {
+        ids: ["test/subject", "test/control"],
+        entities: {
+          "test/subject": {
+            id: "test/subject",
+            color: "blue",
+            translation: 1,
+            scale: 0.7,
+            angle: Math.PI / 3,
+            depth: 5,
+          },
+          "test/control": {
+            id: "test/control",
+            color: "red",
+            translation: 0.8,
+            scale: 0.5,
+            angle: -Math.PI / 4,
+            depth: 7,
+          },
+        },
+      },
+    }
+
+    const result = selectorForDepth("test/subject")(state)
+
+    expect(result).toEqual(5)
   })
 })
--- src/state/recursionBehaviors/selectors.ts
+++ src/state/recursionBehaviors/selectors.ts
@@ -8,6 +8,19 @@ import { RecursionBehaviorId } from "./types"
 const selectRecursionBehaviors = (state: RootState) => state.recursionBehaviors
 
 /**
+ * Selects a list containing all of the existing recursion behaviors.
+ * Results are ordered according to the entity adaptor's `ids` field.
+ */
+const selectAll = recursionBehaviors.getSelectors(selectRecursionBehaviors)
+  .selectAll
+
+/**
+ * Selects the ordered list of recursion behavior IDs.
+ */
+const selectIds = recursionBehaviors.getSelectors(selectRecursionBehaviors)
+  .selectIds
+
+/**
  * Returns a selector for the identified recursion behavior.
  *
  * @param id - The ID of the recursion behavior to select.
@@ -21,4 +34,70 @@ const selectorForRecursionBehavior = (id: RecursionBehaviorId) => (
     .getSelectors(selectRecursionBehaviors)
     .selectById(state, id)
 
-export { selectorForRecursionBehavior }
+/**
+ * Returns a selector for the identified recursion behavior's translation.
+ *
+ * @param id - The ID of the recursion behavior to select from.
+ *
+ * @returns A selector which returns the translation from the identified recursion behavior when called.
+ */
+const selectorForTranslation = (id: RecursionBehaviorId) => (
+  state: RootState
+) => {
+  const recursionBehavior = recursionBehaviors
+    .getSelectors(selectRecursionBehaviors)
+    .selectById(state, id)
+  return (recursionBehavior && recursionBehavior.translation) || undefined
+}
+
+/**
+ * Returns a selector for the identified recursion behavior's scale.
+ *
+ * @param id - The ID of the recursion behavior to select from.
+ *
+ * @returns A selector which returns the scale from the identified recursion behavior when called.
+ */
+const selectorForScale = (id: RecursionBehaviorId) => (state: RootState) => {
+  const recursionBehavior = recursionBehaviors
+    .getSelectors(selectRecursionBehaviors)
+    .selectById(state, id)
+  return (recursionBehavior && recursionBehavior.scale) || undefined
+}
+
+/**
+ * Returns a selector for the identified recursion behavior's angle.
+ *
+ * @param id - The ID of the recursion behavior to select from.
+ *
+ * @returns A selector which returns the angle from the identified recursion behavior when called.
+ */
+const selectorForAngle = (id: RecursionBehaviorId) => (state: RootState) => {
+  const recursionBehavior = recursionBehaviors
+    .getSelectors(selectRecursionBehaviors)
+    .selectById(state, id)
+  return (recursionBehavior && recursionBehavior.angle) || undefined
+}
+
+/**
+ * Returns a selector for the identified recursion behavior's depth.
+ *
+ * @param id - The ID of the recursion behavior to select from.
+ *
+ * @returns A selector which returns the depth from the identified recursion behavior when called.
+ */
+const selectorForDepth = (id: RecursionBehaviorId) => (state: RootState) => {
+  const recursionBehavior = recursionBehaviors
+    .getSelectors(selectRecursionBehaviors)
+    .selectById(state, id)
+  return (recursionBehavior && recursionBehavior.depth) || undefined
+}
+
+export {
+  selectAll,
+  selectIds,
+  selectorForRecursionBehavior,
+  selectorForTranslation,
+  selectorForScale,
+  selectorForAngle,
+  selectorForDepth,
+}
