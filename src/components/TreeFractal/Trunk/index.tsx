1853e091b01642909fab4cc332a026725d23b562
Nick <nick@nickmccrea.com>
2020-05-18 22:12:17 -0400

    Implement `<Trunk />` component.

---
+++ src/components/TreeFractal/Trunk/index.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Branch from "../Branch"
+
+const Trunk = () => (
+  <div style={{ width: "10px", height: "200px", position: "relative" }}>
+    <Branch
+      currentDepth={0}
+      branchPattern={{
+        id: "trunk",
+        color: "black",
+        translation: 1,
+        scale: 1,
+        angle: 0,
+        depth: Infinity,
+      }}
+    />
+  </div>
+)
+
+export default Trunk
---
+++ src/components/TreeFractal/Trunk/trunk.test.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import { BranchPattern } from "../../../state/branchPatterns/types"
+import Trunk from "."
+
+const MOCK_BRANCH_PATTERNS: { [id: string]: BranchPattern } = {
+  "test/a": {
+    id: "test/a",
+    translation: 0.994,
+    scale: 0.543,
+    angle: -3.012 * Math.PI,
+    depth: 3,
+    color: "green",
+  },
+  "test/b": {
+    id: "test/b",
+    translation: 1.042,
+    scale: 0.452,
+    angle: 1.902 * Math.PI,
+    depth: 2,
+    color: "red",
+  },
+}
+
+jest.mock("../../../state/branchPatterns/selectors", () => ({
+  selectAll: () => Object.values(MOCK_BRANCH_PATTERNS),
+}))
+
+jest.mock("react-redux", () => ({
+  useSelector: (selector: Function) => selector(),
+}))
+
+describe("<Trunk />", () => {
+  it("renders correctly", () => {
+    const { container } = render(<Trunk />)
+
+    expect(container).toMatchSnapshot()
+  })
+})
