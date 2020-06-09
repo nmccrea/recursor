4aef1fd60cd182193521d7057f99b63c9eb7f301
Nick <nick@nickmccrea.com>
2020-06-08 21:27:10 -0400

    Make the fractal the homepage.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
-import Image from "../components/Image"
 import SEO from "../components/SEO"
+import TreeFractal from "../components/TreeFractal"
 
-const IndexPage = () => (
+const TreeFractalPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Recursor" />
+    <TreeFractal />
   </Layout>
 )
 
-export default IndexPage
+export default TreeFractalPage
--- src/pages/tree-fractal.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import TreeFractal from "../components/TreeFractal"
-
-const TreeFractalPage = () => <TreeFractal />
-
-export default TreeFractalPage
