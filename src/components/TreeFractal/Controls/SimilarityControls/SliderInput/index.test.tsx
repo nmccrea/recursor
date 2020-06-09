bddc78bcc35112a173a2b21a9fa83e0d45ab4f33
Nick <nick@nickmccrea.com>
2020-06-08 20:16:50 -0400

    Fix broken `<SliderInput />` tests.

--- src/components/TreeFractal/Controls/SimilarityControls/SliderInput/index.test.tsx
+++ src/components/TreeFractal/Controls/SimilarityControls/SliderInput/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { render, fireEvent, getByLabelText } from "@testing-library/react"
+import { render, fireEvent, getByRole } from "@testing-library/react"
 import { createStore, RootState } from "../../../../../state/store"
 import {
   getTranslationSetterFor,
@@ -33,20 +33,56 @@ describe("`<SliderInput />`", () => {
     const { container } = render(
       <Provider store={createStore(state)}>
         <SliderInput
-          selector={getAngleSelectorFor("test/subject")}
-          actionCreator={getAngleSetterFor("test/subject")}
           label="Test Subject - Angle"
           min={0.422}
           max={0.744}
           step={0.001}
+          selector={getAngleSelectorFor("test/subject")}
+          actionCreator={getAngleSetterFor("test/subject")}
+          valueFormatter={jest.fn((value) => String(value))}
         />
         <SliderInput
-          selector={getTranslationSelectorFor("test/subject")}
-          actionCreator={getTranslationSetterFor("test/subject")}
           label="Test Subject - Translation"
           min={0.824}
           max={0.562}
           step={0.001}
+          selector={getTranslationSelectorFor("test/subject")}
+          actionCreator={getTranslationSetterFor("test/subject")}
+          valueFormatter={jest.fn((value) => String(value))}
+        />
+      </Provider>
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it("formats the value using to the given formatter", () => {
+    const state: RootState = {
+      similarities: {
+        ids: ["test/subject"],
+        entities: {
+          "test/subject": {
+            id: "test/subject",
+            translation: 0.6732,
+            scale: 0.8592,
+            angle: 0.1144,
+            depth: 3,
+            color: "violet",
+          },
+        },
+      },
+    }
+
+    const { container } = render(
+      <Provider store={createStore(state)}>
+        <SliderInput
+          label="Test Input"
+          min={0}
+          max={10}
+          step={0.1}
+          selector={getTranslationSelectorFor("test/subject")}
+          actionCreator={getTranslationSetterFor("test/subject")}
+          valueFormatter={() => "fake formatted value"}
         />
       </Provider>
     )
@@ -74,17 +110,18 @@ describe("`<SliderInput />`", () => {
     const { container } = render(
       <Provider store={createStore(state)}>
         <SliderInput
-          selector={getTranslationSelectorFor("test/subject")}
-          actionCreator={getTranslationSetterFor("test/subject")}
           label="Test Input"
           min={0}
           max={10}
           step={0.1}
+          selector={getTranslationSelectorFor("test/subject")}
+          actionCreator={getTranslationSetterFor("test/subject")}
+          valueFormatter={jest.fn((value) => String(value))}
         />
       </Provider>
     )
 
-    fireEvent.change(getByLabelText(container, "Test Input"), {
+    fireEvent.change(getByRole(container, "slider"), {
       target: { value: 0.5582 },
     })
 
