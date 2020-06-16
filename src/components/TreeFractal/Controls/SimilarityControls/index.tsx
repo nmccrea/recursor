2d739e36caf8cf58b2ae60e6fe45cc5c42226e67
Nick <nick@nickmccrea.com>
2020-06-15 20:53:33 -0400

    Implement flexible input containers.

--- src/components/TreeFractal/Controls/SimilarityControls/index.tsx
+++ src/components/TreeFractal/Controls/SimilarityControls/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { formatNumber, formatAngle } from "../../../../utils/valueFormatter"
 import {
   getTranslationSetterFor,
@@ -16,55 +17,74 @@ import { SimilarityId } from "../../../../state/similarities/types"
 import RemoveSimilarityButton from "./RemoveSimilarityButton"
 import SliderInput from "./SliderInput"
 
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 10px;
+`
+
+const Inputs = styled.div`
+  flex-grow: 1;
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: baseline;
+  justify-content: space-evenly;
+`
+
 interface SimilarityControlsProps {
   similarityId: SimilarityId
 }
 
 const SimilarityControls = ({ similarityId }: SimilarityControlsProps) => (
-  <div style={{ display: "flex" }}>
-    <RemoveSimilarityButton similarityId={similarityId} />
+  <Container>
+    <div>
+      <RemoveSimilarityButton similarityId={similarityId} />
+    </div>
 
-    <SliderInput
-      label="Angle"
-      // `min` and `max` must both be multiples of `step`
-      min={-Math.round((2 * Math.PI) / 0.0001) * 0.0001}
-      max={Math.round((2 * Math.PI) / 0.0001) * 0.0001}
-      step={0.0001}
-      selector={getAngleSelectorFor(similarityId)}
-      actionCreator={getAngleSetterFor(similarityId)}
-      valueFormatter={formatAngle}
-    />
+    <Inputs>
+      <SliderInput
+        label="Angle"
+        // `min` and `max` must both be multiples of `step`
+        min={-Math.round((2 * Math.PI) / 0.0001) * 0.0001}
+        max={Math.round((2 * Math.PI) / 0.0001) * 0.0001}
+        step={0.0001}
+        selector={getAngleSelectorFor(similarityId)}
+        actionCreator={getAngleSetterFor(similarityId)}
+        valueFormatter={formatAngle}
+      />
 
-    <SliderInput
-      label="Scale"
-      min={0}
-      max={2}
-      step={0.05}
-      selector={getScaleSelectorFor(similarityId)}
-      actionCreator={getScaleSetterFor(similarityId)}
-      valueFormatter={(value) => formatNumber(value, { precision: 2 })}
-    />
+      <SliderInput
+        label="Scale"
+        min={0}
+        max={2}
+        step={0.05}
+        selector={getScaleSelectorFor(similarityId)}
+        actionCreator={getScaleSetterFor(similarityId)}
+        valueFormatter={(value) => formatNumber(value, { precision: 2 })}
+      />
 
-    <SliderInput
-      label="Translation"
-      min={0}
-      max={1.5}
-      step={0.05}
-      selector={getTranslationSelectorFor(similarityId)}
-      actionCreator={getTranslationSetterFor(similarityId)}
-      valueFormatter={(value) => formatNumber(value, { precision: 2 })}
-    />
+      <SliderInput
+        label="Translation"
+        min={0}
+        max={1.5}
+        step={0.05}
+        selector={getTranslationSelectorFor(similarityId)}
+        actionCreator={getTranslationSetterFor(similarityId)}
+        valueFormatter={(value) => formatNumber(value, { precision: 2 })}
+      />
 
-    <SliderInput
-      label="Depth"
-      min={0}
-      max={8}
-      step={1}
-      selector={getDepthSelectorFor(similarityId)}
-      actionCreator={getDepthSetterFor(similarityId)}
-      valueFormatter={(value) => formatNumber(value, { precision: 0 })}
-    />
-  </div>
+      <SliderInput
+        label="Depth"
+        min={0}
+        max={8}
+        step={1}
+        selector={getDepthSelectorFor(similarityId)}
+        actionCreator={getDepthSetterFor(similarityId)}
+        valueFormatter={(value) => formatNumber(value, { precision: 0 })}
+      />
+    </Inputs>
+  </Container>
 )
 
 export default SimilarityControls
