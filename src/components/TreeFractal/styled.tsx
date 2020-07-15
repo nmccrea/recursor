53b9a81b2bead3fad32181c630f21cea1e2cbb26
Nick <nick@nickmccrea.com>
2020-07-15 17:02:14 -0400

    Use more robust page sizing styles.

--- src/components/TreeFractal/styled.tsx
+++ src/components/TreeFractal/styled.tsx
@@ -3,9 +3,9 @@ import styled from "styled-components"
 const Container = styled.div`
   position: absolute;
   top: 0px;
-  bottom: 0px;
   left: 0px;
-  right: 0px;
+  width: 100vw;
+  height: 100vh;
 
   display: grid;
   grid-template-rows: 2fr 1fr;
