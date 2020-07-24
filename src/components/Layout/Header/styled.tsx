575e4bbbb99a210e83c1eb06923c7750d21e792e
Nick <nick@nickmccrea.com>
2020-07-23 20:53:02 -0400

    Improve styling of `<Header />`.

--- src/components/Layout/Header/styled.tsx
+++ src/components/Layout/Header/styled.tsx
@@ -6,13 +6,12 @@ export const H1 = styled.h1`
   ${typography.fontFamilyHeading}
   color: ${color.primary};
   text-transform: uppercase;
-  line-height: 0;
-  padding: 0.5rem 1rem;
+  line-height: 1;
+  margin: 0;
 `
 
 export const HeaderPanel = styled(Panel)`
   position: fixed;
   top: 1rem;
   left: 1rem;
-  padding: 0;
 `
